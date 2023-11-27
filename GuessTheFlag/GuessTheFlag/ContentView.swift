bb05532f6805caaf94fb42d0a9e54f72e578ae9f
munchangjae <107340769+munchangjae@users.noreply.github.com>
2023-11-28 00:01:29 +0900

    Button & Image

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,7 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    func executeDelete(){
+        print("deleting,,,")
+    }
     var body: some View {
+        
         TabView {
                     // 첫 번째 탭
             ZStack{
@@ -23,19 +27,74 @@ struct ContentView: View {
                     
                     // 두 번째 탭
             VStack{
-                LinearGradient(colors: [.red,.black], startPoint: .top, endPoint: .bottom)
-                LinearGradient(stops: [
-                    Gradient.Stop(color: .blue, location: 0.5),
-                    Gradient.Stop(color: .black, location: 0.5),
-                ], startPoint: .top, endPoint: .bottom)
-                RadialGradient(colors: [.green,.gray], center: .center, startRadius: 20, endRadius: 100)
+                ScrollView{
+                    LinearGradient(colors: [.red,.black], startPoint: .top, endPoint: .bottom)
+                        .frame(minWidth: 200, minHeight: 200)
+                    LinearGradient(stops: [
+                        Gradient.Stop(color: .blue, location: 0.5),
+                        Gradient.Stop(color: .black, location: 0.5),
+                    ], startPoint: .top, endPoint: .bottom)
+                    .frame(minWidth: 200, minHeight: 200)
+                    RadialGradient(colors: [.green,.gray], center: .center, startRadius: 20, endRadius: 100)
+                        .frame(minWidth: 200, minHeight: 200)
+                    AngularGradient(colors: [.red, .yellow, .green, .blue, .purple, .red], center: .center)
+                        .frame(minWidth: 200, minHeight: 200)
+                    Text("Your content")
+                        .frame(minWidth: 200, minHeight: 200)
+                        .foregroundStyle(.white)
+                        .background(.red.gradient)
+                       
+                }
             }
             
-            //어떤 색, 방향, 타입 지정
                         .tabItem {
                             Image(systemName: "2.circle")
                             Text("Gradients")
                         }
+            ScrollView{
+                Button("Delete selection", role: .destructive, action: executeDelete)
+                
+                VStack {
+                    Button("Button 1") { }
+                        .buttonStyle(.bordered)
+                    Button("Button 2", role: .destructive) { }
+                        .buttonStyle(.bordered)
+                    Button("Button 3") { }
+                        .buttonStyle(.borderedProminent)
+                        .tint(.mint)
+                    Button("Button 4", role: .destructive) { }
+                        .buttonStyle(.borderedProminent)
+                }
+                Button {
+                    print("Button was tapped")
+                } label: {
+                    Text("Tap me!")
+                        .padding()
+                        .foregroundStyle(.white)
+                        .background(.red)
+                }
+                Button{
+                    //action
+                }label: {
+                    Image(systemName: "pencil")
+                }
+                
+                Button{
+                    //action
+                }label: {
+                    Label("LabelType", systemImage: "pencil")
+                        .padding()
+                        .background(.black)
+                        .foregroundColor(.white)
+                        
+                        
+                }
+            }
+            
+                .tabItem {
+                    Image(systemName: "3.circle")
+                    Text("Button and Image")
+                }
                 }
     }
 }
