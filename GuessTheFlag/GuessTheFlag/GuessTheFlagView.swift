267bd8c91b98956bbd74eaa8a3d213f5e6d4e98d
munchangjae <107340769+munchangjae@users.noreply.github.com>
2023-11-29 23:43:39 +0900

    - Guess The Flag app complete

--- GuessTheFlag/GuessTheFlag/GuessTheFlagView.swift
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagView.swift
@@ -4,30 +4,77 @@
 import SwiftUI
 
 struct GuessTheFlagView: View {
-    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"]
-    var correctAnswer = Int.random(in: 0...2)
+    
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
     // 0, 1, 2
+    
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Wrong"
+        }
+
+        showingScore = true
+    }
+    
+    func askQuestion(){
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
     
     var body: some View {
         ZStack{
-            Color.blue
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
+            ], center: .top, startRadius: 200, endRadius: 400)
                 .ignoresSafeArea()
-            VStack(spacing: 30) {
-                VStack {
-                        Text("Tap the flag of")
-                        .foregroundStyle(.white)
-                        Text(countries[correctAnswer])
-                        .foregroundStyle(.white)
-                    }
-                
-                ForEach(0..<3) { number in
-                        Button {
-                           // flag was tapped
-                        } label: {
-                            Image(countries[number])
+            VStack{
+                Spacer()
+                Text("Guess The Flag")
+                    .font(.largeTitle.bold())
+                    .foregroundStyle(.white)
+                Spacer()
+                Text("Score: ???")
+                    .foregroundStyle(.white)
+                    .font(.title.bold())
+                Spacer()
+                VStack(spacing: 15) {
+                    VStack {
+                            Text("Tap the flag of")
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline.weight(.heavy))
+
+                            Text(countries[correctAnswer])
+                            .foregroundStyle(.secondary)
+                            .font(.largeTitle.weight(.semibold))
                         }
+                    
+                    ForEach(0..<3) { number in
+                            Button {
+                               flagTapped(number)
+                            } label: {
+                                Image(countries[number])
+                                    .cornerRadius(5)
+                                    .shadow(radius: 5)
+                            }
                     }
-            }
+                    
+                }.frame(maxWidth: .infinity)
+                    .padding(.vertical, 20)
+                    .background(.regularMaterial)
+            }.padding()
+            
+            
+        }.alert(scoreTitle, isPresented: $showingScore){
+            Button("Continue", action: askQuestion)
+        }message: {
+            Text("Your score is ???")
         }
         
         
