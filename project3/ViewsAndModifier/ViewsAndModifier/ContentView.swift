b5d54d0eda52bb3ff4bad02552a417cc4fd68d44
munchangjae <107340769+munchangjae@users.noreply.github.com>
2023-12-04 23:59:41 +0900

    - project3

---
+++ project3/ViewsAndModifier/ViewsAndModifier/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ViewsAndModifier
+//
+//  Created by 문창재 on 2023/12/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ project3/ViewsAndModifier/ViewsAndModifier/ViewsAndModifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifierApp.swift
+//  ViewsAndModifier
+//
+//  Created by 문창재 on 2023/12/04.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
