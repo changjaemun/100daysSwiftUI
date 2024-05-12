37c77a778b7e6cf14823408f9c9581dd19728b43
문창재 <ckdwo23@gmail.com>
2024-05-13 04:08:06 +0900

    Day 29

---
+++ project5/WordScramble/WordScramble/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by 문창재 on 5/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        List {
+            Section("Section 1") {
+                Text("Static row 1")
+                Text("Static row 2")
+            }
+
+            Section("Section 2") {
+                ForEach(0..<5) {
+                    Text("Dynamic row \($0)")
+                }
+            }
+
+            Section("Section 3") {
+                Text("Static row 3")
+                Text("Static row 4")
+            }
+        }.listStyle(.grouped)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ project5/WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by 문창재 on 5/13/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
