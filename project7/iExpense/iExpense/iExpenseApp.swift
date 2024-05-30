3beba9f5891e1788ff5053a8ca31df505dcff767
문창재 <ckdwo23@gmail.com>
2024-05-30 17:31:39 +0900

    project8 챌린지 3번 제외하고 완성

---
+++ project7/iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by 문창재 on 5/22/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
